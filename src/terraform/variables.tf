4e8ba33f11101814096f4f8660148f5b646c6bb3
Mark Tinderholt <markti@outlook.com>
2025-04-26 15:43:30 -0400

    Managed by Terraform

---
+++ src/terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "application_name" {
+  type = string
+}
+variable "environment_name" {
+  type = string
+}
+variable "primary_location" {
+  type = string
+}
+variable "base_address_space" {
+  type = string
+}
+variable "additional_regions" {
+  type = map(string)
+  default = {}
+}
+variable "vpn_address_space" {
+  type = string
+}
+variable "additional_tags" {
+  type = map(string)
+  default = {}
+}
